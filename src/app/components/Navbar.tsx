33abb8dd14a6bde9db52bb3c77fc3b89b0923566
PauloMats <pm.mats98@gmail.com>
2024-06-28 14:00:48 -0300

    Adiciona layout inicial e estilização

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,3 +1,4 @@
+// src/app/components/navbar.tsx
 import Link from 'next/link';
 import './Navbar.css';
 
@@ -5,12 +6,24 @@ export default function NavBar() {
   return (
     <nav className="navbar">
       <div className="div01">
-        <div className="div02"> Pibtv </div>
+        <div className="div02">Primeira Igreja Batista em Teotônio Vilela</div>
         <div className="div03">
           <ul className="ul01">
             <li>
               <Link href="/" className="inicio">Início</Link>
             </li>
+            {/* <li>
+              <Link href="#visit" className="visit">Visite-nos</Link>
+            </li>
+            <li>
+              <Link href="#media-library" className="mediaLibrary">Mídia PIBTV</Link>
+            </li>
+            <li>
+              <Link href="#about" className="about">Sobre nós</Link>
+            </li>
+            <li>
+              <Link href="#events" className="events">Eventos e Avisos</Link>
+            </li> */}
             <li>
               <Link href="/members" className="members">Membros</Link>
             </li>
@@ -18,6 +31,9 @@ export default function NavBar() {
               <Link href="/members/add" className="members_add">Adicionar Membro</Link>
             </li>
           </ul>
+        </div>
+        <div className="menu">
+          <button>☰</button>
         </div>
       </div>
     </nav>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,28 @@
+// src/app/page.tsx
+"use client";
 
-export default function Home() {
+import styles from './Home.module.css';
+
+const Home: React.FC = () => {
   return (
-  <h1>Site Oificial da PIBTV</h1>
+    <div>
+    <div className={styles.container}></div>
+      <section className={styles.banner}>
+        <h1>Jesus está aqui, Tudo é possível ao que crê! </h1>
+        <p>CULTO AOS <b>DOMINGO</b> ÀS 18H | <b>TERÇA-FEIRAS</b> ÀS 19H30 </p>
+        <div className={styles.socialIcons}>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br /> 
+          <p>Nossas redes sociais</p>
+          <a href="#instagram">📸</a>
+          <a href="#youtube">📺</a>
+        </div>
+      </section>
+    </div>
   );
-}
+};
+
+export default Home;
