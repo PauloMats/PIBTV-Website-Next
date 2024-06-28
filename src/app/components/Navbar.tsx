648790b389f32467284648cb338e8937cef39344
PauloMats <pm.mats98@gmail.com>
2024-06-28 10:42:46 -0300

    Add navbar and css

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import './Navbar.css';
+
+export default function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="div01">
+        <div className="div02"> Pibtv </div>
+        <div className="div03">
+          <ul className="ul01">
+            <li>
+              <Link href="/" className="inicio">Início</Link>
+            </li>
+            <li>
+              <Link href="/members" className="members">Membros</Link>
+            </li>
+            <li>
+              <Link href="/Info" className="info">Informações</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
