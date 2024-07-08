a93e1ed086b7b5115322cd5452eaaead63119635
PauloMats <pm.mats98@gmail.com>
2024-07-08 15:43:41 -0300

    Feat: Adiciona imagens das celulas e estilização da page celulas

--- src/app/celulas/page.tsx
+++ src/app/celulas/page.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import Image from 'next/image';
+import image1 from '../../assets/celula encontro comunhao.png';
+import image2 from '../../assets/celula prototipo encontro.png';
+import image3 from '../../assets/Edificação Celulas.png';
+import './celulas.css'
 
-const Celulas = () => {
-    return (
-        <div>
-            <h1>Welcome to My Website</h1>
-            <img src="image1.jpg" alt="Image 1" />
-            <img src="image2.jpg" alt="Image 2" />
-            <img src="image3.jpg" alt="Image 3" />
-        </div>
-    );
-};
+    const Celulas = () => {
+        return (
+            <div className="container">
+                <h1>Conheça nossa Célula Protótipo</h1>
+                <div className="image-gallery">
+                    <div className="image-container">
+                        <Image className="image" src={image1} alt="Image 1" />
+                    </div>
+                    <div className="image-container">
+                        <Image className="image" src={image2} alt="Image 2" />    
+                    </div>
+                    <div className="image-container">
+                        <Image className="image" src={image3} alt="Image 3" />
+                    </div>
+                </div>
+            </div>
+        );
+    };
 
 export default Celulas;
