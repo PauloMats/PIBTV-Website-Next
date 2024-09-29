c0f17846029956909cfed2e05d979024c52b2ecd
PauloMats <pm.mats98@gmail.com>
2024-09-29 15:09:18 -0300

    Feat: Adiciona mais informações, estilização e footer na página

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,17 +14,16 @@ const Home: React.FC = () => {
       <section>
       <div className='image-page-container'>
       <Image
+              className="image-page"
               src={fotoBemVindos}
               alt="Imagem da igreja"
-              style={{ objectFit: 'cover', width: '100%', height: '100%' }} // Estilo inline para garantir comportamento desejado
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             />
   </div>
       </section>
     </div>
     <section className="info-page1">
       <h1>Bem-vindos à Primeira Igreja Batista em Teotônio Vilela</h1>
-      <p>Seja bem-vindo ao site da Primeira Igreja Batista em Teotônio Vilela. Aqui você encontra informações sobre nossos cultos, eventos e atividades. Fique à vontade para navegar e conhecer mais sobre a nossa igreja.</p>
+      <p>Seja bem-vindo e bem-vinda ao site da Primeira Igreja Batista em Teotônio Vilela. Aqui você encontra informações sobre nossos cultos, eventos e atividades. Fique à vontade para navegar e conhecer mais sobre a nossa igreja.</p>
   </section>  
   <section className="info-page2">
       <h1>Culto todos os Domingo!</h1>
@@ -32,6 +31,9 @@ const Home: React.FC = () => {
         Nossos cultos são realizados todos os domingos às 18h00. Venha participar conosco e ouvir a Palavra de Deus.
       </p>
   </section>  
+  <footer className='footer-page'>
+    <p>PIBTV © 2024 - Todos os Direitos Reservado</p>
+  </footer>
     </>
   );  
 };
